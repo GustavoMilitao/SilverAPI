a40da2ca445493c902468cf580490104eebdbac2
Militão <gustavo.h.militao@gmail.com>
2017-08-07 14:21:00 -0300

    Alterações no modelo de dados na parte de pagamento e implementação do PaymentMethod (somente a entidade)

---
+++ SilverEntities/PaymentMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverEntities
+{
+    public class PaymentMethod
+    {
+        public int ID { get; set; }
+        public int ID_User { get; set; }
+        public int ID_Pay_Type { get; set; }
+        public int? ID_Credit_Card { get; set; }
+        public bool? Active { get; set; }
+        public DateTime Reg_Date { get; set; }
+    }
+}
